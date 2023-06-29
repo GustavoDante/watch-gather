1e86eef34a0eade6ec9028fa6d5a9a10cc520c35
GustavoDante <gustavodantespfc@gmail.com>
2023-06-29 01:29:56 -0300

    test: debbug in production

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import { SearchSection } from '@/sections/SearchSection'
-// import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 
 export default async function SearchPage() {
   const language = 'pt-BR'
-  // const searchParams = useSearchParams()
-  // const title = searchParams.get('title') // get the param from the URL
-
+  const searchParams = useSearchParams()
+  const title = searchParams.get('title') // get the param from the URL
+  console.log(title)
   return (
     <>
       {/* @ts-expect-error */}
