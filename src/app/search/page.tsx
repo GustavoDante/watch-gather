ea58c6f73fff1891215d1f87aecf8485c2a7c48a
GustavoDante <gustavodantespfc@gmail.com>
2023-06-29 01:03:53 -0300

    fix: test

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,17 +1,15 @@
-'use client'
-
 import { SearchSection } from '@/sections/SearchSection'
-import { useSearchParams } from 'next/navigation'
+// import { useSearchParams } from 'next/navigation'
 
 export default async function SearchPage() {
   const language = 'pt-BR'
-  const searchParams = useSearchParams()
-  const title = searchParams.get('title') // get the param from the URL
+  // const searchParams = useSearchParams()
+  // const title = searchParams.get('title') // get the param from the URL
 
   return (
     <>
       {/* @ts-expect-error */}
-      <SearchSection language={language} title={title} />
+      <SearchSection language={language} title={'teste'} />
     </>
   )
 }
