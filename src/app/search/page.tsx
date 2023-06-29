5ea5fabd41a6542a59a4636862563e1127464e15
GustavoDante <gustavodantespfc@gmail.com>
2023-06-29 17:51:37 -0300

    feat: test

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,17 +6,18 @@ import { useEffect } from 'react'
 
 export default function SearchPage() {
   const language = 'pt-BR'
+
   const searchParams = useSearchParams()
-  const title = searchParams.get('title')
+  // const title = searchParams.get('title')
 
   useEffect(() => {
-    console.log(title)
-  }, [title])
+    console.log(searchParams)
+  }, [searchParams])
 
   return (
     <>
       {/* @ts-expect-error */}
-      <SearchSection language={language} title={title} />
+      <SearchSection language={language} title={'title'} />
     </>
   )
 }
