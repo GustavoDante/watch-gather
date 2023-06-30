3576da36418c48d1321a68cfdf270479e35c14e2
GustavoDante <gustavodantespfc@gmail.com>
2023-06-30 14:01:18 -0300

    feat: test

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,6 +5,15 @@ export default function SearchPage(params: {
   searchParams: { title: string }
 }) {
   const language = 'pt-BR'
+  const request = params
+
+  if (!request || !request.searchParams || !request.searchParams.title) {
+    return (
+      <div className="mt-10 w-10/12">
+        <h2 className="text-1 mb-1">Nenhum resultado encontrado</h2>
+      </div>
+    )
+  }
 
   return (
     <>
