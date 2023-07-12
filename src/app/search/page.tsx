bd316289bbfc18c1d6ce2bb7cc5253e84cefe9e8
GustavoDante <gustavodantespfc@gmail.com>
2023-07-12 19:02:08 -0300

    test

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,28 +1,3 @@
-// import { SearchSection } from '@/sections/SearchSection'
-
-// export default function SearchPage(params: {
-//   params: {}
-//   searchParams: { title: string }
-// }) {
-//   const language = 'pt-BR'
-//   const request = params
-
-//   if (!request || !request.searchParams || !request.searchParams.title) {
-//     return (
-//       <div className="mt-10 w-10/12">
-//         <h2 className="text-1 mb-1">Nenhum resultado encontrado</h2>
-//       </div>
-//     )
-//   }
-
-//   return (
-//     <>
-//       {/* @ts-expect-error */}
-//       <SearchSection language={language} title={params.searchParams.title} />
-//     </>
-//   )
-// }
-
 'use client'
  
 import { useSearchParams } from 'next/navigation'
@@ -32,8 +7,6 @@ export default function SearchPage() {
  
   const search = searchParams.get('search')
  
-  // This will be logged on the server during the initial render
-  // and on the client on subsequent navigations.
   console.log(search)
  
   return <>Search: {search}</>
